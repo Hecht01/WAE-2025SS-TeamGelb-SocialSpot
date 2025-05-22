ebc2e3a911e0636f0616e5580be8a2d6755af4d4
Yannick <y.ondra@oth-aw.de>
2025-05-22 11:52:32 +0200

    add social spot logo

--- sys-src/app/frontend/SocialSpot/src/routes/map/+page.svelte
+++ sys-src/app/frontend/SocialSpot/src/routes/map/+page.svelte
@@ -8,7 +8,7 @@
 </style>
 
 
-<h1 class="sosp-header">SocialSpot - Kartenansicht</h1>
+<img src="logo.png" alt="Social Spot Logo" style="max-width: 400px; margin: 2rem auto; display: block;" />
 <Map />
 
 <button class="sosp-button" on:click={() => goto('/')}>Zurück</button>
